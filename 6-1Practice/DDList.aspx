770fa41539819ae54de5c99563a0c4bd2f4d347e
康恆 徐 <wa0125678665@>
2021-06-20 02:06:26 +0800

    done

--- 6-1Practice/DDList.aspx
+++ 6-1Practice/DDList.aspx
@@ -7,10 +7,12 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
 </head>
-<body>
+    <body>
     <form id="form1" runat="server">
+        <asp:DropDownList ID="ddl_Area" AutoPostBack="true" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="ddl_Place" runat="server"></asp:DropDownList>
         <div>
         </div>
     </form>
-</body>
+    </body>
 </html>
--- 6-1Practice/DDList.aspx.cs
+++ 6-1Practice/DDList.aspx.cs
@@ -8,7 +8,27 @@ using System.Web.UI.WebControls;
 namespace _6_1Practice {
     public partial class DDList : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            if (ddl_Area.Items.Count == 0)
+            {
+                ddl_Area.Items.Clear();
+                ddl_Area.Items.Add("北區");
+                ddl_Area.Items.Add("中區");
+            }
+            if (ddl_Area.SelectedValue == "北區")
+            {
+                ddl_Place.Items.Clear();
+                ddl_Place.Items.Add(new ListItem("基隆", "基隆"));
+                ddl_Place.Items.Add(new ListItem("台北", "台北"));
+                ddl_Place.Items.Add(new ListItem("新北", "新北"));
+            }
+            else if (ddl_Area.SelectedValue == "中區")
+            {
+                ddl_Place.Items.Clear();
+                ddl_Place.Items.Add(new ListItem("苗栗", "苗栗"));
+                ddl_Place.Items.Add(new ListItem("台中", "台中"));
+                ddl_Place.Items.Add(new ListItem("南投", "南投"));
 
+            }
         }
     }
 }
